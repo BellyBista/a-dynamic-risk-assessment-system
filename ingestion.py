0dd5677e7145ddb6ca744aac32d7d2553c6390a4
quadribello <quadribello41@gmail.com>
2023-05-29 19:08:45 -0400

    run ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -1,11 +1,14 @@
+"""
+Author: Quadri Bello
+Date: May, 29th 2023
+This script used for ingesting data
+"""
+
 import pandas as pd
 import numpy as np
 import os
 import json
 from datetime import datetime
-
-
-
 
 #############Load config.json and get input and output paths
 with open('config.json','r') as f:
@@ -14,12 +17,54 @@ with open('config.json','r') as f:
 input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
 
+# get current directory
+working_dir = os.getcwd()
+# get current date
+today = datetime.today().strftime('%Y%m%d')
 
+def read_csv(filename):
+    """ read a csv file using filename
+    input: filenames to read
+    output: dataframe
+    """
+    return pd.read_csv(filename)
 
 #############Function for data ingestion
 def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+    """ 
+    combine multiple datasets into one master file
+    record ingested files and save to disk
+    input: None
+    output: Master dataset and list of ingested files saved to disk
+    """
 
+    # master dataset placeholder
+    finaldata = pd.DataFrame()
+    # ingested files placeholder
+    ingestedfiles = []
+
+    #check for datasets
+    files = os.listdir(input_folder_path)
+
+    # compile datasets together and store ingested file names
+    for file in files:
+        filepath = os.path.join(input_folder_path,file)
+        temp = read_csv(filepath)
+        finaldata = pd.concat([finaldata,temp], axis=0)
+        ingestedfiles.append(file)
+
+    # drop duplicates
+    finaldata.drop_duplicates(inplace=True)
+
+    # write dataset to an output master file
+    savepath = os.path.join(output_folder_path,'finaldata.csv')
+    finaldata.to_csv(savepath, index=False)
+
+    # save ingested files with timestamp
+    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), "w") as f:
+        f.write(
+            f"Ingestion date: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
+        f.write(str(ingestedfiles))    
 
 
 if __name__ == '__main__':
